6f8a44f631a0d138c7b25076a1b148991dc15fd9
LLla6aJl <salimovvvv@mail.ru>
2022-05-04 15:09:10 +0300

    didmount

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import store from 'store';
 
 import './index.scss';
 import App from './components/app/app';
 
+store.clearAll();
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
